72d118b3f0039bb28aa7a5cc9ceffc00773aea4f
AnaelDamette <damette.anael@gmail.com>
2021-09-11 11:37:24 +0200

    mise en place du serveur

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,6 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', userRoutes);
 app.use('/api/post', postRoutes);
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const postCtrl = require('../controllers/post');
-const multer = require('../middleware/multer-config');
 
 
 router.get("/", auth, postCtrl.listMsg);
 router.get("/mesMessages/:uuid", auth, postCtrl.listMesMsg);
-router.post("/create/:uuid", auth, multer, postCtrl.createMsg);
+router.post("/create/:uuid", auth, postCtrl.createMsg);
 router.delete('/delete/:uuid', auth, postCtrl.deleteMsg);
-router.put("/update/:uuid", auth, multer, postCtrl.updateMsg);
+router.put("/update/:uuid", auth, postCtrl.updateMsg);
 
 module.exports = router;
