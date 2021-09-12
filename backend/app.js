d8e1aab78d26fd9a43f9cab70d52d9af1d93fb03
AnaelDamette <damette.anael@gmail.com>
2021-09-12 10:52:13 +0200

    mise en place back de la route /api/TempHumidity

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/user.js');
-const tempHumidityRoutes = requite('./routes/tempHumidity.js')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user.js");
+const tempHumidityRoutes = require("./routes/tempHumidity.js");
+const path = require("path");
 const app = express();
-const { Sequelize, sequelize } = require('./models')
+const { Sequelize, sequelize } = require("./models");
 
- async function main() {
-   await sequelize.authenticate({ force:true })
- }
+async function main() {
+  await sequelize.authenticate({ force: true });
+}
 
-main()
+main();
 try {
-    sequelize.authenticate();
-    console.log('Connecté à la base de données MySQL!');
-  } catch (error) {
-    console.error('Impossible de se connecter, erreur suivante :', error);
-  }
+  sequelize.sync();
+  //sequelize.authenticate();
+  console.log("Connecté à la base de données MySQL!");
+} catch (error) {
+  console.error("Impossible de se connecter, erreur suivante :", error);
+}
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
 app.use(bodyParser.json());
 
-app.use('/api/auth', userRoutes);
-app.use('/api/tempHumidity', tempHumidityRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/tempHumidity", tempHumidityRoutes);
 module.exports = app;
--- backend/controllers/tempHumidity.js
+++ backend/controllers/tempHumidity.js
@@ -2,17 +2,19 @@ exports.getTempHumidity = (req, res, next) => {
   let ping = req.body.ping;
   let sensor = require("node-dht-sensor");
   if (ping === true) {
+  let tempHumidity = [];
     sensor
       .read(22, 13, function (error, temperature, humidity) {
         if (!error) {
           console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
           tempHumidity = [temperature, humidity];
+          return res.status(200).json(tempHumidity)
         } else {
           console.log("oupsy, le capteur est Broken Cheh !" + tempHumidity);
+          error = "Avez-vous bien branché votre Capteur ? "
+          return res.status(500).json(error)
         }
       })
-      .then((tempHumidity) => res.status(200).json(tempHumidity))
-      .catch((error) => res.status(500).json(error));
   } else {
       console.log("next")
     next();
